bc1f0ecf97d0a5fddbd89e137807fbefb845518a
preforegoer <123918009+preforegoer@users.noreply.github.com>
2023-02-07 14:27:19 +0600

    Add files via upload

---
+++ Task 15 - Weekends/Program.cs
@@ -0,0 +1,12 @@
+﻿// Weekend - Error if Not 
+Console.Clear();
+Console.WriteLine("Enter a Number:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a > 7 || a == 0)
+    Console.WriteLine("1 Week = 7 Days (From 1 to 7).");
+
+else if (a < 6)
+    Console.WriteLine("Not a Weekend.");
+else
+    Console.WriteLine("Weekend.");
