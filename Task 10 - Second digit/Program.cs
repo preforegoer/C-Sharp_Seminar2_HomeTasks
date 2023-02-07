789eec35fb95f362beea79811e26955a06b796d8
preforegoer <123918009+preforegoer@users.noreply.github.com>
2023-02-07 14:25:36 +0600

    Add files via upload

---
+++ Task 10 - Second digit/Program.cs
@@ -0,0 +1,14 @@
+﻿// Second digit - 456:5, 782:8, 918:1
+Console.Clear();
+Console.WriteLine("Enter a 3-digit number:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a;
+
+if (b > 99 & b < 1000)
+{
+    b = (a - (a/100*100 + a%10))/10;
+    Console.WriteLine($"Second digit is {b}.");
+}
+    
+else
+    Console.WriteLine("Not a 3-digit number.");
