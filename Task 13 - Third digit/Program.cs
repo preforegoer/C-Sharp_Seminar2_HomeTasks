271a258c26f25b66f9d236970d630ef1abc49f75
preforegoer <123918009+preforegoer@users.noreply.github.com>
2023-02-07 14:26:43 +0600

    Add files via upload

---
+++ Task 13 - Third digit/Program.cs
@@ -0,0 +1,13 @@
+﻿// Third digit - 645:5, 78:Error, 32679:6
+Console.Clear();
+Console.WriteLine("Enter a Number:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a;
+
+while (b > 999)
+    b = b / 10;
+
+if (a < 99)
+    Console.WriteLine("None");
+else
+    Console.WriteLine(b%10);
